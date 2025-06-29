5fcf5603fdf3db894df3337501dd1fc5aa48ab15
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-06-28 20:37:46 -0500

    local

--- internal/main.go
+++ internal/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
-	"github.com/guidomantilla/yarumo/pkg/server"
+
 	"github.com/samber/lo"
+
+	clog "github.com/guidomantilla/yarumo/pkg/common/log"
+	"github.com/guidomantilla/yarumo/pkg/server"
 )
 
 func main() {
+	options := clog.Chain().WithCaller(false).Build()
 	server.Run("yarumo-app", "1.0.0", func(ctx context.Context, app server.Application) error {
 
 		_ = lo.Empty[int]()
 
 		return nil
-	})
+	}, options)
 }
