2e5e532beb2ca40b4966040e4ce23375dc771d71
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-07-06 16:37:25 -0500

    updates

--- pkg/common/comm/http_round_tripper_circuit_breaker.go
+++ pkg/common/comm/http_round_tripper_circuit_breaker.go
@@ -3,21 +3,17 @@ package comm
 import (
 	"net/http"
 
-	"github.com/sony/gobreaker"
+	gobreaker "github.com/sony/gobreaker/v2"
 )
 
 type HttpCircuitBreakerRoundTripper struct {
-	Breaker *gobreaker.CircuitBreaker
+	Breaker *gobreaker.CircuitBreaker[*http.Response]
 	Next    http.RoundTripper
 }
 
 func (tripper *HttpCircuitBreakerRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	newReq := req.Clone(req.Context())
-	var resp *http.Response
-	_, err := tripper.Breaker.Execute(func() (any, error) {
-		r, err := tripper.Next.RoundTrip(newReq)
-		resp = r
-		return r, err
+	return tripper.Breaker.Execute(func() (*http.Response, error) {
+		return tripper.Next.RoundTrip(newReq)
 	})
-	return resp, err
 }
