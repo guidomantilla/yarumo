8129f02ed6b6ee79cf37e5f31def87c2a8c9dadd
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-11-07 22:29:50 -0500

    updates

--- modules/common/graph.go
+++ modules/common/graph.go
@@ -6,12 +6,6 @@ import (
 	_ "github.com/guidomantilla/yarumo/common/constraints"
 	_ "github.com/guidomantilla/yarumo/common/errs"
 	_ "github.com/guidomantilla/yarumo/common/log"
-	//_ "github.com/guidomantilla/yarumo/common/maths/logic/engine"
-	//_ "github.com/guidomantilla/yarumo/common/maths/logic/entailment"
-	//_ "github.com/guidomantilla/yarumo/common/maths/logic/parser"
-	//_ "github.com/guidomantilla/yarumo/common/maths/logic/props"
-	//_ "github.com/guidomantilla/yarumo/common/maths/logic/sat"
-	//_ "github.com/guidomantilla/yarumo/common/maths/sets"
 	_ "github.com/guidomantilla/yarumo/common/pointer"
 	_ "github.com/guidomantilla/yarumo/common/utils"
 )
