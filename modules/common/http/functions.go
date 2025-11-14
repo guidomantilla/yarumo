3f16d047f4838786a3ea08dd96d4fcb6c61e6b54
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-11-14 12:15:41 -0500

    updates

--- modules/common/http/functions.go
+++ modules/common/http/functions.go
@@ -1,5 +1,12 @@
 package http
 
+import "github.com/avast/retry-go/v4"
+
+var (
+	_ retry.RetryIfFunc = NoopRetryIf
+	_ retry.OnRetryFunc = NoopRetryHook
+)
+
 func NoopRetryIf(_ error) bool {
 	return false
 }
