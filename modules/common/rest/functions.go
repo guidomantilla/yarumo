9949d76b7873b178310879d18e56b8f520615c18
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-11-22 11:02:08 -0500

    updates

--- modules/common/rest/functions.go
+++ modules/common/rest/functions.go
@@ -1,13 +1,63 @@
 package rest
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"mime"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/guidomantilla/yarumo/common/assert"
 	"github.com/guidomantilla/yarumo/common/utils"
 )
+
+// Call executes a request and returns the response.
+func Call[T any](ctx context.Context, spec *RequestSpec, options ...Option) (*ResponseSpec[T], error) {
+	assert.NotEmpty(ctx, "ctx is nil")
+	assert.NotEmpty(spec, "spec is nil")
+
+	opts := NewOptions(options...)
+	req, err := spec.Build(ctx)
+	if err != nil {
+		return nil, ErrCall(err)
+	}
+
+	start := time.Now()
+	resp, err := opts.DoFn(req)
+	if err != nil {
+		return nil, ErrCall(err)
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, ErrCall(err)
+	}
+
+	duration := time.Since(start)
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, ErrCall(&HTTPError{StatusCode: resp.StatusCode, Status: resp.Status, Body: body})
+	}
+
+	decoded, err := decodeResponseBody[T](body, resp.StatusCode, resp.Header.Get("Content-Type"))
+	if err != nil {
+		return nil, ErrCall(err)
+	}
+
+	return &ResponseSpec[T]{
+		Duration:      duration,
+		ContentLength: int64(len(body)),
+		Headers:       resp.Header.Clone(),
+		Code:          resp.StatusCode,
+		Status:        resp.Status,
+		Body:          decoded,
+	}, nil
+}
 
 // isJSONMediaType checks if the media type is JSON.
 func isJSONMediaType(mediaType string) bool {
--- modules/common/rest/types.go
+++ modules/common/rest/types.go
@@ -3,7 +3,7 @@ package rest
 import "context"
 
 var (
-	_ CallFn[any] = Call[any]
+	_ CallFn[any] = Call
 )
 
 type CallFn[T any] func(ctx context.Context, spec *RequestSpec, options ...Option) (*ResponseSpec[T], error)
