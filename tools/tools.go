68daa90c3fbd861355dabc468fee21a3168c0557
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-11-27 09:06:56 -0500

    updates

--- tools/tools.go
+++ tools/tools.go
@@ -5,9 +5,9 @@ package tools
 
 import (
 	_ "github.com/Knetic/govaluate"
+	_ "github.com/brianvoe/gofakeit"
 	/*
 		_ "github.com/bradfitz/gomemcache/memcache"
-		_ "github.com/brianvoe/gofakeit/v7"
 		_ "github.com/core-go/mq"
 		_ "github.com/emirpasic/gods/utils"
 		_ "github.com/grpc-ecosystem/go-grpc-middleware/v2"
@@ -19,7 +19,6 @@ import (
 		_ "github.com/kelindar/timeline"
 		_ "github.com/mehdihadeli/go-mediatr"
 		_ "github.com/montanaflynn/stats"
-		_ "github.com/pquerna/otp" //Check https://medium.com/@sanhdoan/implementing-totp-authentication-in-go-a-practical-guide-6fe39a349035
 		_ "github.com/reugn/go-streams"
 		_ "github.com/samber/lo"
 		_ "github.com/shopspring/decimal"
