af99bf97cdef7d4943b6919a445aec27826f50dc
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-11-30 16:05:55 -0500

    updates

--- modules/common/crypto/ciphers/aead/aead.go
+++ modules/common/crypto/ciphers/aead/aead.go
@@ -2,14 +2,13 @@ package aead
 
 import (
 	"github.com/guidomantilla/yarumo/common/crypto/ciphers/aead/internal"
-	"golang.org/x/crypto/chacha20poly1305"
 )
 
 var (
 	AES_128_GCM        = NewMethod("AES_128_GCM", 16, 12, internal.AESGCM)
 	AES_256_GCM        = NewMethod("AES_256_GCM", 32, 12, internal.AESGCM)
-	CHACHA20_POLY1305  = NewMethod("CHACHA20_POLY1305", chacha20poly1305.KeySize, chacha20poly1305.NonceSize, internal.ChaCha20Poly1305)
-	XCHACHA20_POLY1305 = NewMethod("XCHACHA20_POLY1305", chacha20poly1305.KeySize, chacha20poly1305.NonceSizeX, internal.XChaCha20Poly1305)
+	CHACHA20_POLY1305  = NewMethod("CHACHA20_POLY1305", 32, 12, internal.ChaCha20Poly1305)
+	XCHACHA20_POLY1305 = NewMethod("XCHACHA20_POLY1305", 32, 24, internal.XChaCha20Poly1305)
 )
 
 type Method struct {
