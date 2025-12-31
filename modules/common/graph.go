1de09c06688cdd055bf4d95586c4d1ad5fd83828
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2025-12-31 17:57:13 -0500

    updates

--- modules/common/graph.go
+++ modules/common/graph.go
@@ -14,7 +14,7 @@ import (
 	_ "github.com/guidomantilla/yarumo/common/errs"
 	_ "github.com/guidomantilla/yarumo/common/grpc"
 	_ "github.com/guidomantilla/yarumo/common/http"
-	_ "github.com/guidomantilla/yarumo/common/log"
+	//_ "github.com/guidomantilla/yarumo/common/log"
 	_ "github.com/guidomantilla/yarumo/common/pointer"
 	_ "github.com/guidomantilla/yarumo/common/random"
 	_ "github.com/guidomantilla/yarumo/common/rest"
