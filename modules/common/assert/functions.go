78ab4aefc66b4460ee088b4807dab8b2ed492cf8
Guido Mantilla Tarazona <raven@macbook-pro-m1-max.local>
2026-01-01 18:39:50 -0500

    updates

--- modules/common/assert/functions.go
+++ modules/common/assert/functions.go
@@ -1,10 +1,10 @@
 package assert
 
 import (
-	"os"
+	"fmt"
 	"sync/atomic"
 
-	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/guidomantilla/yarumo/common/utils"
 )
@@ -15,46 +15,63 @@ func Enable(v bool) {
 	enabled.Store(v)
 }
 
-var logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
-
 // NotEmpty checks if the object is not empty and logs a fatal error if it is.
 func NotEmpty(object any, message string) {
-	if enabled.Load() && utils.Empty(object) {
-		logger.Fatal().Msg(message)
+	if utils.Empty(object) {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: object is empty")).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: object is empty")).Msg(message)
+		}
 	}
+
 }
 
 // NotNil checks if the object is not nil and logs a fatal error if it is.
 func NotNil(object any, message string) {
-	if enabled.Load() && utils.Nil(object) {
-		logger.Fatal().Msg(message)
+	if utils.Nil(object) {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: object is nil")).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: object is empty")).Msg(message)
+		}
 	}
 }
 
 // Equal checks if two values are equal and logs a fatal error if they are not.
 func Equal(val1 any, val2 any, message string) {
-	if enabled.Load() && utils.NotEqual(val1, val2) {
-		logger.Fatal().Msg(message)
+	if utils.NotEqual(val1, val2) {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: %s != %s", val1, val2)).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: %s != %s", val1, val2)).Msg(message)
+		}
 	}
 }
 
 // NotEqual checks if two values are not equal and logs a fatal error if they are.
 func NotEqual(val1 any, val2 any, message string) {
-	if enabled.Load() && utils.Equal(val1, val2) {
-		logger.Fatal().Msg(message)
+	if utils.Equal(val1, val2) {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: %s == %s", val1, val2)).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: %s == %s", val1, val2)).Msg(message)
+		}
 	}
 }
 
 // True checks if a condition is true and logs a fatal error if it is not.
 func True(condition bool, message string) {
-	if enabled.Load() && !condition {
-		logger.Fatal().Msg(message)
+	if !condition {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: condition is false")).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: condition is false")).Msg(message)
+		}
 	}
 }
 
 // False checks if a condition is false and logs a fatal error if it is not.
 func False(condition bool, message string) {
-	if enabled.Load() && condition {
-		logger.Fatal().Msg(message)
+	if condition {
+		log.Logger.Error().Err(fmt.Errorf("assertion failed: condition is true")).Msg(message)
+		if enabled.Load() {
+			log.Logger.Fatal().Err(fmt.Errorf("assertion failed: condition is true")).Msg(message)
+		}
 	}
 }
